1937df591d1465cc0515678e8e9e7c926a4a7fca
Pelanyo Kamara <pkamara@pkamara.me>
2020-08-16 01:46:52 +0100

    fix again :eyes:

--- src/compiler/utils/exception.h
+++ src/compiler/utils/exception.h
@@ -1,17 +1,17 @@
 #pragma once
 
-#include <exception>
+#include <stdexcept>
 #include <string>
 
 #include "position.h"
 
 namespace lang::compiler
 {
-	struct exception : public std::exception
+	struct exception : public std::runtime_error
 	{
 		position pos;
 
-		explicit exception(position pos, const std::string& msg) : std::exception(msg.c_str()),
+		explicit exception(position pos, const std::string& msg) : std::runtime_error(msg.c_str()),
 			pos(pos)
 		{}
 	};
